30ef795cd8340b1abd42adc3f289bbf636ce0fb6
objektwerks <objektwerks@runbox.com>
2024-01-18 15:21:20 -0500

    enhanced index.ts

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
-console.log("*** Hello, Typescript! ***")
+function log(message: string): void { console.log(message) }
+
+log("*** Hello, Typescript! ***")
 
 console.log("*** Types ***")
 const explicit: string = "explicit"
 const implicit = "implicit" // or type inferred
-console.log("const explicit type: " + explicit)
-console.log("const implicit type: " + implicit)
+log("const explicit type: " + explicit)
+log("const implicit type: " + implicit)
 
 let number = 3
-console.log(`let number (${number}) squared: ${number * number}`)
+log(`let number (${number}) squared: ${number * number}`)
 number = 4
-console.log(`let number (${number}) cubed: ${number * number * number}`)
+log(`let number (${number}) cubed: ${number * number * number}`)
