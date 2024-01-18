880fe8c60764635d8d5a143cb691dc1b8abd6ca3
objektwerks <objektwerks@runbox.com>
2024-01-18 15:10:34 -0500

    edited index.ts

--- index.ts
+++ index.ts
@@ -1,2 +1,7 @@
 console.log("*** Hello, Typescript! ***")
 
+console.log("*** Types ***")
+const explicit: string = "explicit"
+const implicit = "implicit" // or type inferred
+console.log("explicit type: " + explicit)
+console.log("implicit type: " + implicit)
